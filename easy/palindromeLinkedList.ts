58db23eca67ebd8b707cdee3c4ff915b2897c935
blazejzj <opexrrator@gmail.com>
2025-05-22 21:33:07 +0200

    feat: Linked list palindrome easy

---
+++ easy/palindromeLinkedList.ts
@@ -0,0 +1,43 @@
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+    const arr: number[] = [];
+    while (head) {
+        arr.push(head.val);
+        head = head.next;
+    }
+
+    let left = 0,
+        right = arr.length - 1;
+
+    while (left < right) {
+        if (arr[left] !== arr[right]) return false;
+        right--;
+        left++;
+    }
+    return true;
+}
+
+// console.log(isPalindrome(new ListNode(1, new ListNode(2, null))));
+
+console.log(
+    isPalindrome(
+        new ListNode(
+            1,
+            new ListNode(
+                2,
+                new ListNode(
+                    3,
+                    new ListNode(3, new ListNode(2, new ListNode(1, null)))
+                )
+            )
+        )
+    )
+);
