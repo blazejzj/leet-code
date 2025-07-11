4f0a5743f19637048a92b2bd8725e6c1163e4f27
blazejzj <opexrrator@gmail.com>
2025-07-11 11:45:22 +0200

    feat: cells with odd values in a matrix 1252 easy

---
+++ easy/cellsWithOddValuesInAMatrix1252.ts
@@ -0,0 +1,31 @@
+function oddCells(m: number, n: number, indices: number[][]): number {
+    let cols: number[] = Array(n).fill(0);
+    let rows: number[] = Array(m).fill(0);
+
+    for (let [ri, ci] of indices) {
+        rows[ri] += 1;
+        cols[ci] += 1;
+    }
+
+    let odd: number = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if ((rows[i] + cols[j]) % 2 === 1) {
+                odd++;
+            }
+        }
+    }
+
+    console.log(cols);
+    console.log(rows);
+
+    return odd;
+}
+
+console.log(
+    oddCells(2, 3, [
+        [0, 1],
+        [1, 1],
+    ])
+);
